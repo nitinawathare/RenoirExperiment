393a556852fecd4a0b63410a38008603fb5e5812
shashisingh111 <shashiraz111@gmai.com>
2020-01-21 12:51:45 +0530

    updated code

--- TransactionDependency.py
+++ TransactionDependency.py
@@ -5,37 +5,26 @@ from pathlib import Path
 
 def main():
 
-	p = 40
+	p = 20
 	adjList = []
 	blockfile = "/home/shashi/RenoirExperiment/gitRepoRenoir/Block1/"
-	# with open(blockfile) as bf:
-	for block in os.listdir(blockfile):
-		# for block in bf and i in range(10):
-		
+	for block in os.listdir(blockfile):	
 		blockInfo = []
 		# Reading block one by one.....
 		file = '/home/shashi/RenoirExperiment/gitRepoRenoir/Block1/'+block
-		# print(file)
 		blockRead = []
 		blockWrite = []
 
 		if Path(file).exists():
 			with open(file) as f:
+				# Reading trx. one by one in block.....
 				for txnum, tl in enumerate(f):
 					tl = tl.rstrip('\n')
-					# txrw = block+"_"
-					# print(txrw)
 					rwfile = str( '/home/shashi/RenoirExperiment/gitRepoRenoir/transactionsRW/'+str(block)+'_'+str(tl) )
-					# print(rwfile)
 					trRead = []
 					trWrite = []
 					
-					if not Path(rwfile).exists():
-						print("file not exists")
-					else:
-						# print("file not exists")
-
-
+					if  Path(rwfile).exists():
 						with open(rwfile) as rw:
 							for data in rw:
 								mystring = data.split("_")
@@ -45,16 +34,12 @@ def main():
 									trRead.append((mystring[1], value))
 								else:
 									trWrite.append((mystring[1], value))
-						# for j in trRead:
-						# 	print(j)
-						# for k in trWrite:
-						# 	print(k)
-
-
 						check = 0
+						
+						# checking for readset present in writeset of previously executed transaction.............
+						# checking readset of Tx(N) in executed transactions Tx(1.....N-1) in reverse manner , if readset present in writeset of Tx(1...N-1) then Tx(N) is dependent on TX(M).
 						for i, e in reversed(list(enumerate(blockWrite))):
 							for j in trRead:
-							# 	print(j)
 								if j in e:
 									blockInfo.append((txnum, i))
 									check = 1
@@ -64,27 +49,21 @@ def main():
 								break
 						if check == 0:
 							blockInfo.append((txnum, "_" ))
+					else:
+						blockInfo.append((txnum,"file not exist"))		
 					blockWrite.append(trWrite)
-			# for j in blockWrit:
-			# 	print(j)
-			# 	print("\n")		
-			adjList.append(blockInfo)
+			a=blockInfo[:]	
+			adjList.append(a)
 			blockInfo.clear()
-			
-		else:
-			continue
+		
 		p = p+1
-		print("block done..........................")
-		if(p == 50):
+		# print("block done..........................")
+		if(p == 40):
 			break
 	for s in adjList:
-		print(*s) 
+		print(s) 
 
 
-
-
-
-				
 
 if __name__ == "__main__":
     main()
