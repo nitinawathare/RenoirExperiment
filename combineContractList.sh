a2c3cafa189e7f0df90e56e313b00c0ea40a98db
Nitin <nitinawathare14@gmail.com>
2020-02-09 00:37:23 +0530

    update to calculate the latest transaction that has been included in block

--- combineContractList.sh
+++ combineContractList.sh
@@ -6,8 +6,8 @@ SERVER_LIST=ipList
 while read REMOTE_SERVER
 do
 	#echo $REMOTE_SERVER
-	ssh -n -i quorum2.key ubuntu@$REMOTE_SERVER "cat /home/ubuntu/gitRepoEVD/contractAddress" &
-	#echo $REMOTE_SERVER " : " $(ssh -n -i quorum2.key ubuntu@$REMOTE_SERVER "cat /home/ubuntu/gitRepoEVD/contractAddress") & 
-	#ssh -n -i quorum2.key ubuntu@$REMOTE_SERVER "> /home/ubuntu/gitRepoEVD/contractAddress" &
-	#scp -i quorum2.key downloadEVDCodeGoEthereumSkip2.sh ubuntu@$REMOTE_SERVER:/home/ubuntu/gitRepoEVD/ &
+	ssh -n -i quorum2.key ubuntu@$REMOTE_SERVER "cat /home/ubuntu/gitRepoRenoir/contractAddress" &
+	#echo $REMOTE_SERVER " : " $(ssh -n -i quorum2.key ubuntu@$REMOTE_SERVER "cat /home/ubuntu/gitRepoRenoir/contractAddress") & 
+	#ssh -n -i quorum2.key ubuntu@$REMOTE_SERVER "> /home/ubuntu/gitRepoRenoir/contractAddress" &
+	#scp -i quorum2.key downloadEVDCodeGoEthereumSkip2.sh ubuntu@$REMOTE_SERVER:/home/ubuntu/gitRepoRenoir/ &
 done < $SERVER_LIST
