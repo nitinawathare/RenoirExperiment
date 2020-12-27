033442ba0fef0a7945ace2fe5f6aede100048054
sourav1547 <souravdas1547@gmail.com>
2020-12-27 12:46:32 +0530

    automated plot generation

---
+++ graph/data/consistency.py
@@ -0,0 +1,14 @@
+
+
+def main():
+	advs = [1/10,3/10,1/2]
+	cvalues = [1,2,4,10,25,60,100]
+
+	for c in range(0,len(cvalues)):
+		p = 1/(cvalues[c]*10^13)
+vdxvxv
+		print(p)
+
+
+if __name__ == "__main__":
+	main()
---
+++ script/runScripts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python3 ethereumFairnessGraph.py
+python3 forkRate.py eth 10
+# python3 peerSimilarity.py <path to the data>
+# python3 propagationDelay.py <path to the data>
